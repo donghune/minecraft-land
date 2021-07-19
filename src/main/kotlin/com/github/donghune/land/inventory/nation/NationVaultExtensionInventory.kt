39e553545f731a8e15e2c235c9700f18553d73e4
yudonghun <donghun15.yu@samsung.com>
2021-07-19 17:56:08 +0900

    add: 추가된 GUI 클래스만 생성

---
+++ src/main/kotlin/com/github/donghune/land/inventory/nation/NationVaultExtensionInventory.kt
@@ -0,0 +1,42 @@
+package com.github.donghune.land.inventory.nation
+
+import com.github.donghune.namulibrary.extension.ItemBuilder
+import com.github.donghune.namulibrary.inventory.GUI
+import com.github.donghune.plugin
+import org.bukkit.Material
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.inventory.InventoryOpenEvent
+import org.bukkit.inventory.ItemStack
+
+class NationVaultExtensionInventory : GUI(plugin, 27, "금고 확장") {
+
+    companion object {
+        private val ICON_GOLD: () -> ItemStack = {
+            ItemBuilder()
+                .setMaterial(Material.GOLD_INGOT)
+                .setDisplay("")
+                .setLore(
+                    listOf(
+                        ""
+                    )
+                )
+                .build()
+        }
+    }
+
+    override suspend fun onInventoryClose(event: InventoryCloseEvent) {
+    }
+
+    override suspend fun onInventoryOpen(event: InventoryOpenEvent) {
+    }
+
+    override suspend fun onPlayerInventoryClick(event: InventoryClickEvent) {
+    }
+
+    override suspend fun setContent() {
+        setItem(10, ICON_GOLD()) {
+            it.isCancelled = true
+        }
+    }
+}
